6c3195dbdb3413952c1937273a1b733576a304f1
Antish Kumar Yadav <antishyadav726@gmail.com>
2026-03-14 15:45:08 +0530

    controller module added

---
+++ controllers/listings.js
@@ -0,0 +1,59 @@
+const Listing = require('../models/listing')
+
+module.exports.index = async (req, res) => {
+    const allListings = await Listing.find({}); // Fetch all listings from DB
+    res.render("listings/index.ejs", { allListings }); // Render listings/index.ejs
+}
+
+module.exports.renderForm = (req, res) => {
+  res.render("listings/new.ejs"); // Show new listing form
+}
+
+module.exports.showListingsById = async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id)
+      .populate({
+        path: "reviews",
+        populate: {
+          path: "author",
+        },
+      })
+      .populate({ path: "owner" });
+    // Find listing by ID
+    if (!listing) {
+      req.flash("error", "Listing not found");
+      res.redirect("/listings");
+    }
+    // console.log(listing);
+    res.render("listings/show.ejs", { listing }); // Render show page
+  }
+
+  module.exports.createNewListing=async (req, res, next) => {
+      const newListing = new Listing(req.body.listing); // Create new Listing object
+      newListing.owner = req.user._id;
+      await newListing.save(); // Save to database
+      req.flash("success", "New Listing Created");
+      res.redirect("/listings"); // Redirect to all listings
+    }
+
+    module.exports.editListing=async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id); // Find listing to edit
+    req.flash("success", "Listing edited");
+    res.render("listings/edit.ejs", { listing }); // Render edit form
+  }
+
+  module.exports.updateListing = async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing }); // Update listing
+    req.flash("success", "Listing Updated");
+    res.redirect(`/listings/${id}`); // Redirect to updated listing
+  }
+
+  module.exports.deleteListing = async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id); // Delete by ID
+    console.log(deletedListing); // Log deleted document
+    req.flash("success", "Listing Deleted");
+    res.redirect("/listings"); // Redirect to all listings
+  }
---
+++ controllers/user.js
@@ -0,0 +1,52 @@
+const User = require("../models/user.js");
+
+module.exports.signUpPage = (req, res) => {
+  res.render("users/signup.ejs");
+}
+
+module.exports.signUp= async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      let newUser = new User({
+        username,
+        email,
+      });
+      const registerUser = await User.register(newUser, password);
+      console.log(registerUser);
+      req.login(registerUser, (req, res, next) => {
+        if (err) {
+          return next(err);
+        }
+        req.flash("success", "user registered");
+        res.redirect("/listings");
+      });
+    } catch (error) {
+      req.flash("error", error.message);
+      res.redirect("/signup");
+    }
+  }
+
+  module.exports.loginPage = (req, res) => {
+  res.render("users/login.ejs");
+  }
+
+  module.exports.login = async (req, res) => {
+    try {
+      let redirectUrl = res.locals.redirectUrl || '/listings' 
+      req.flash("success", "Welcome back");
+      res.redirect(redirectUrl);
+    } catch (error) {
+      req.flash("error", error.message);
+      res.redirect("/login");
+    }
+  }
+
+  module.exports.logout= (req, res, next) => {
+  req.logout((err) => {
+    if(err){
+        return next(err);
+    }
+    req.flash("success", "you are logout!");
+    res.redirect("/login");
+  });
+}
